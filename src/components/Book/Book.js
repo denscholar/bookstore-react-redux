81ee3eff74e00a90fa39fedef1f8793345c91994
Dennis Akagha <d.akagha20@gmail.com>
2022-03-11 10:33:10 +0100

    Add and remove book feature

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,52 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Container, Col, Row } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import PropTypes from 'prop-types';
 import './Book.css';
+import { removeBookAction } from '../../redux/books/books';
 
-const Book = ({
-  title,
-  category,
-  author,
-  percentage,
-}) => (
-  <Container className="Lesson-Panel">
-    <li>
-      <Row className="row-item">
-        <Col sm>
-          <p>{category}</p>
-          <h3>{title}</h3>
-          <p>{author}</p>
-          <button type="button">Comment</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </Col>
-        <Col sm className="status">
-          <CircularProgressbar className="oval-2" value={percentage} />
-          <div className="completed-item">
-            <p className="percentage">
-              {percentage}
-              %
-            </p>
-            <p className="completed">Completed</p>
-          </div>
-        </Col>
-        <Col sm>
-          <h3 className="current-chapter">CURRENT CHAPTER</h3>
-          <p className="current-lesson">Chapeter 17</p>
-          <button type="button" className="btn-book">UPDATE PROGRESS</button>
-        </Col>
-      </Row>
-    </li>
-  </Container>
-);
+const Book = (props) => {
+  const { title, category, id } = props;
+  const dispatch = useDispatch();
+  const percentage = 60;
+
+  // Remove book
+  const handleRemove = (id) => {
+    dispatch(removeBookAction(id));
+  };
+  return (
+    <Container className="Lesson-Panel">
+      <li>
+        <Row className="row-item">
+          <Col sm>
+            <p>{category}</p>
+            <h3>{title}</h3>
+            <p>James Doe</p>
+            <button type="button">Comment</button>
+            <button type="button" onClick={() => handleRemove(id)}>Remove</button>
+            <button type="button">Edit</button>
+          </Col>
+          <Col sm className="status">
+            <CircularProgressbar className="oval-2" value={percentage} />
+            <div className="completed-item">
+              <p className="percentage">
+                {percentage}
+                %
+              </p>
+              <p className="completed">Completed</p>
+            </div>
+          </Col>
+          <Col sm>
+            <h3 className="current-chapter">CURRENT CHAPTER</h3>
+            <p className="current-lesson">Chapeter 17</p>
+            <button type="button" className="btn-book">UPDATE PROGRESS</button>
+          </Col>
+        </Row>
+      </li>
+    </Container>
+  );
+};
 
 Book.propTypes = {
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,40 +1,18 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from '../Book/Book';
-import Form from '../Form/Form';
 
 const Books = () => {
-  const bookList = [
-    {
-      title: 'The Early sun',
-      author: 'John Doe',
-      id: 1,
-      category: 'Action',
-      percentage: 50,
-    },
-    {
-      title: 'The Return of James Doe',
-      author: 'John Doe',
-      id: 2,
-      category: 'Fiction',
-      percentage: 30,
-    },
-    {
-      title: 'Lover Girl',
-      author: 'John Doe',
-      id: 3,
-      category: 'Fiction',
-      percentage: 10,
-    },
-  ];
+  const books = useSelector((state) => state.books);
   return (
     <div>
       <ul>
-        {bookList.map((book) => (
-          <Book key={book.id} title={book.title} percentage={book.percentage} author={book.author} category={book.category} />
-        ))}
+        {books.map((book) => {
+          const { id, title, category } = book;
+          return <Book key={id} id={id} title={title} category={category} />;
+        })}
       </ul>
-      <Form />
     </div>
   );
 };
